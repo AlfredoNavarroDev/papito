6de4866c2fe21714574b83dae02f079f11a910b7
AlfredoNavarroDev <alfredont1088@gmail.com>
2026-06-21 18:29:43 -0500

    chore: remove backend directory and update .gitignore
    Delete app/backend folder (moved to separate repository).
    Add .vercel to .gitignore.

--- app/backend/src/config/database.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require("@prisma/client");
-const { PrismaPg } = require("@prisma/adapter-pg");
-require("dotenv").config({ path: require("path").join(__dirname, "..", "..", ".env") });
-
-const connectionString = process.env.DATABASE_URL;
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
-
-module.exports = prisma;
